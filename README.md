09ef95d008d2bf452b79bc43fe120e9d49120436
Stuart Dear <stuart.dear@intermarkgroup.com>
2022-05-05 14:10:34 -0500

    bare bones component

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Next JS, Typescript, Storybook Garbage
+
+Run Me : npm run dev
+
+Load Storybook : npm run storybook
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,2 +1,8 @@
-const Index = ()=>( <h1>Here i am</h1> );
+import RenderCounter from "../src/components/render-counter";
+
+const Index = ()=>(
+<div>
+    <h1>Here i am</h1>
+    <RenderCounter/>
+</div> );
 export default Index;
