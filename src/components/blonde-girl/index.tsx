7a74ddf1f79074c6ffa0cf3da140370a86db5a56
Stuart Dear <stuart.dear@intermarkgroup.com>
2022-06-15 14:56:05 -0500

    added useState and onclick handler  with TS type.

--- src/components/blonde-girl/index.tsx
+++ src/components/blonde-girl/index.tsx
@@ -1,19 +1,26 @@
-import React, { FC, ReactElement } from 'react';
+import React, { FC, ReactElement, useState } from 'react';
 import styles from "./blonde-girl.module.scss";
 
 type BlondeGirlProps = {
   name: string,
   id: number,
   bio?: string,
+  initialClick?: number,
 }
 
-const BlondeGirl: FC<BlondeGirlProps> = ({name, id, bio = 'Bio empty'}): ReactElement => {
+const BlondeGirl: FC<BlondeGirlProps> = ({name, id, bio = 'Bio empty', initialClick = 0}): ReactElement => {
+  const [click, setClick] = useState(initialClick);
   return (
     <div className={styles["blonde-girl"]}>
     <div className={styles["blonde-girl__bio"]} data-testid={"blonde-girl__bio"}>
       <h2>Name: {name}, Id: {id}</h2>
       <p>{bio}</p>
+      <div className={styles["blonde-girl__clickArea"]} data-testid={"blonde-girl__clickArea"}>
+        <p>Click: {click}</p>
+        <button onClick={() => setClick(click + 1)}>Click Me</button>
+        </div>
     </div>
+
     </div>
   )
 };
