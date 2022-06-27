8f778e27208a1340d2312361a170890c39651079
Stuart Dear <stuart.dear@intermarkgroup.com>
2022-06-27 16:08:05 -0500

    wizard bio useState hook, callback function and update

--- src/components/wizard-bio/index.tsx
+++ src/components/wizard-bio/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import styles from './wizard-bio.module.scss';
 
 interface WizardBioProps {
@@ -19,6 +19,17 @@ const WizardImage: FC<WizardImageProps> = ({ url }) => {
 
 
 const WizardBio: FC<WizardBioProps> = ({ name, age, children }) => {
+    // we get initial value for `name` from parent prop,  and set as initial value in useState
+    const nameValueFromProp = name;
+    const [wizardName, setWizardName] = useState(nameValueFromProp);
+
+    const handleNameInputChange = input => {
+        console.log('handling change to Wiz Name input :', input);
+        setWizardName(input);
+
+        // setIsError(false);
+      };
+
     return (
         <div className={styles['wizard-bio']}>
             <div className={styles['wizard-bio__content']}>
@@ -26,6 +37,27 @@ const WizardBio: FC<WizardBioProps> = ({ name, age, children }) => {
                 <p>Hello {name} </p>
                 <p>Age : {age} </p>
                 <WizardImage url="cool"/>
+                <div>Change Name from {nameValueFromProp}?  :
+                    {/* LAST ON HERE WITH THIS DRILL, USESTATE AND USEEFFECT PER STACK ARTICLEK */}
+                <input
+                    className={styles["wizard-bio__input"]}
+                    placeholder="New Name"
+                    data-testid={'wizard-bio__input'}
+                    onChange={e => handleNameInputChange(e.target.value)}
+                />
+                <br />
+                New Name : {wizardName}
+                </div>
+
+
+        {/* ORACLE
+        <input
+         className={styles["alert__input"]}
+         placeholder="Enter here to dismiss alert"
+        data-testid={'alert__input'}
+        onChange={e => handleInputChange(e.target.value)}
+        value={userInput}
+      /> */}
             </div>
         </div>
     )
