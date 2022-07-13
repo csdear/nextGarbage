74e04d97ad6a379a1f28e12b954d49527a3897e6
Stuart Dear <stuart.dear@intermarkgroup.com>
2022-07-13 12:32:32 -0500

    basic framer motion image slider

--- src/components/image-slider/index.tsx
+++ src/components/image-slider/index.tsx
@@ -10,19 +10,40 @@ interface ImageSliderProps {
 const ImageSlider: FC<ImageSliderProps> = ({ MacGuffin='Framer Motion' }) => {
 console.log('img_Arr', images);
 // Create a state which is our width that we need to scroll
-// grab the width of our parent. LO 18:56
+// grab the width of our parent. Outer width (carousel) is much wider though not seen. because of
+// overflow hidden. LO 18:56
+// we will use  useRef to essentially get an element and get alot of information about it.
+// like get the width. like a query selector in vanilla js. Simply create a var for useReef hook
+// and add attribute ref={var} to what you want to monitor.
+// Use a useEffect with an empty dep that will run when compoinent gets mounted. And what you can
+// do is console.log out carousel to see the data properties of the object via thee .current property.
+// interesting too because you could get the  current, must up to date class name, which might be important
+// to you because on render a new class name is gnerated, eg : <div class="image-slider_image-slider__carousel__p4QA8"
+// How much width is needed? One trick is to inspect the element, drag to the last elemnt and note the translate(x) value.
+// around -781 pixels for me.  But if we console log carousel.current.scrollWidth, we get 1138px -- totally  doesnt
+//match that translatex() value of -781 px.  grab the full width (1920) .1920 minus 781 = 1139px
+// if  we console log :  console.log(carousel.current.scrollWidth, carousel.current.offsetWidth); we get
+// 1920 1135. 1920 - 1135 = 785. That sum value is close to our original calculation of 1920
+// with that we can invoke setWidth to  update the state
+// attribute whileTap={"grabbing"} makes it to where cursor rollover the entire canrousel area is a open hand to grabbed hand on click as opposed to a mouse pointer.
 const [width, setWidth] = useState(0);
+const carousel = useRef();
+
+useEffect(() => {
+    console.log(carousel.current.scrollWidth, carousel.current.offsetWidth);
+    setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);  //Sets constraint boundry for Left
+}, [])
 
 return (
     <div className={styles["image-slider"]}>
         <div className={styles["image-slider__someSubDiv"]}>
             <motion.h1 animate={{ x:250 }}>ImageSlider - {MacGuffin}</motion.h1>
-            <motion.div className={styles["image-slider__carousel"]}>
+            <motion.div ref={carousel} className={styles["image-slider__carousel"]}>
                 {/*Add drag to the inner container to move. outer (carousel will not move*/}
-                <motion.div drag="x" dragConstraints={{right: 0}} className={styles["image-slider__inner-carousel"]}>
+                <motion.div drag="x" dragConstraints={{right: 0, left: -width}} whileTap={{ cursor: "grabbing"}} className={styles["image-slider__inner-carousel"]}>
                     {images.map(image => {
                         return(
-                            <motion.div className={styles["image-slider__item"]}>
+                            <motion.div className={styles["image-slider__item"]} key={image.src}>
                                 <img src={image.src} alt="" />
                             </motion.div>
                         );
