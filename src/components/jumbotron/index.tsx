a0bfad98f3215c195b9db3b40c06ac23e1059c3f
Stuart Dear <stuart.dear@intermarkgroup.com>
2022-10-27 15:59:39 -0500

    interesting jumbotron

--- src/components/jumbotron/index.tsx
+++ src/components/jumbotron/index.tsx
@@ -6,7 +6,7 @@ const Jumbotron: FC = () => {
 return (
     <div className={styles["jumbotron"]}>
     <div className={styles["jumbotron__container"]}>
-
+    <div className={styles["jumbotron__image"]}><img src="https://images.pexels.com/photos/694740/pexels-photo-694740.jpeg?auto=compress&cs=tinysrgb&w=1000&h=500&dpr=1"></img></div>
         <div className={styles["jumbotron__copy"]}>
             <h1>Flipster Digital Magazines</h1>
                 <p>Providing readers with access to best-selling  digital magazines from  top publishers including</p>
