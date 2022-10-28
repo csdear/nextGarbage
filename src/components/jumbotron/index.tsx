f7efcf07d9b4857632b03b12468cb53a23b26677
Stuart Dear <stuart.dear@intermarkgroup.com>
2022-10-28 15:10:24 -0500

    jumbotron tweaks

--- src/components/jumbotron/index.tsx
+++ src/components/jumbotron/index.tsx
@@ -1,19 +1,18 @@
-import { FC } from "react";
+// import { FC } from "react";
 import styles from "./jumbotron.module.scss";
 
-const Jumbotron: FC = () => {
+const Jumbotron = () => {
 
 return (
     <div className={styles["jumbotron"]}>
-    <div className={styles["jumbotron__container"]}>
-    <div className={styles["jumbotron__image"]}>
+        <div className={styles["jumbotron__container"]}>
+            <div className={styles["jumbotron__image"]} />
+                <div className={styles["jumbotron__copy"]}>
+                    <h1>Flipster Digital Magazines</h1>
+                        <p>Providing readers with access to best-selling  digital magazines from  top publishers including</p>
+                        <p>National Geographic, Conde Nast, Meredith, Bloomberg L.P. and more.</p>
+                </div>
         </div>
-        <div className={styles["jumbotron__copy"]}>
-            <h1>Flipster Digital Magazines</h1>
-                <p>Providing readers with access to best-selling  digital magazines from  top publishers including</p>
-                <p>National Geographic, Conde Nast, Meredith, Bloomberg L.P. and more.</p>
-        </div>
-    </div>
     </div>
 );
 };
