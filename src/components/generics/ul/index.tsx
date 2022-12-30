61dc9d0738c8e96312c319910e53e63535a226a6
Stuart Dear <stuart.dear@intermarkgroup.com>
2022-12-30 09:19:20 -0600

    some stasis here in the generic component def. Next we will try to implement it.

--- src/components/generics/ul/index.tsx
+++ src/components/generics/ul/index.tsx
@@ -1,21 +1,39 @@
-// import { FC } from "react";
+import { FC } from "react";
 import styles from "./ul.module.scss";
 
+// GENERIC UL
 // Not about to use FC with Generics...
+// And may have to give up the T....
+// My research consensus is that you can't create a functional component with a type annotation and make it generic
+// You could still go the function route if your wanted.
+// Not need React.DetailedHTMLProps either?
+// but can no longer use T[] -- most likely need to  define the array shape for item as well.
+// See notes re ts Pokemon array.
+// from the useTodosHook I brought in todo which has the shape and renamed it item
+// then my ul items can have type item[]
+// next challenged is fixing these T' upd funcctions here.
+// simply doing for render() and itemClick item: item seems to cause no  fuss.
+// borrowing hard from navigation-link
+// I have no use for  ...rest at the moment. But something to look into.
 
-interface ULProps<T> {
-    items: T[];
-    render: (item: T) => React.ReactNode;
-    itemClick: (item: T) => void;
+interface item {
+    id: number;
+    done: boolean;
+    text: string;
+  }
+
+interface ULProps
+    extends React.HTMLAttributes<HTMLUListElement> {
+        items: item[];
+        render: (item: item) => React.ReactNode;
+        itemClick: (item: item) => void;
 }
 
-
-
-const UL = <T extends unknown> ({
+const UL: FC<ULProps> = ({
     items,
     render,
     itemClick
- }: ULProps<T>) => {
+ }) => {
     return (
     <ul>
       {items.map((item, index) => (
