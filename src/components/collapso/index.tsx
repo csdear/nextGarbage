d05483d8b98f4ef11561f74134787fccfa98a2a1
Stuart Dear <stuart.dear@intermarkgroup.com>
2023-03-28 15:22:08 -0500

    progress collapsible EOD... of course problems with font awesome.

---
+++ src/components/collapso/index.tsx
@@ -0,0 +1,50 @@
+import React, { FC, useState, ReactNode } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faChevronUp,
+    faChevronDown
+} from "@fortawesome/fontawesome-free-solid";
+    // } from "@fortawesome/free-solid-svg-icons"; // Try alternative library.
+
+import styles from "./collapso.module.scss";
+
+interface CollapsoProps {
+    open?: boolean;
+    title: string;
+    children: ReactNode;
+    icon?: IconProp;
+}
+
+
+const Collapso: FC<CollapsoProps> = ({ open, title, children }) => {
+    const [isOpen, setIsOpen] = useState(open);
+
+    const handleFilterOpening = () => {
+        setIsOpen((prev) => !prev);
+    };
+
+return (
+    <div className={styles["collapso"]} data-testid={"collapso"}>
+        <div className={styles["collapso__container"]}>
+            <div className={styles["collapso__header"]} data-testid={"collapso-header"}>
+                <h6>{title}</h6>
+            </div>
+            <div className={styles["collapso__content"]} data-testid={"collapso-content"}>
+                <button type="button" className="btn" onClick={handleFilterOpening}>
+                    {!isOpen ? (
+                    <FontAwesomeIcon icon={faChevronDown} />
+                    ) : (
+                    <FontAwesomeIcon icon={faChevronUp} />
+                    )}
+
+                </button>
+            </div>
+
+        </div>
+
+    </div>
+);
+};
+
+export default Collapso;
+
